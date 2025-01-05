7bccd8e40b23151ecf01368a172fff34efec1e19
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2025-01-05 17:42:38 +0600

    Filtering

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,16 +1,47 @@
 import { AddTasksModel } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { selectTasks } from "@/redux/features/Tasks/taskSlice";
-import { useAppSelector } from "@/redux/hook";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Priority } from "@/contants/priorityOptions";
+import { selectTasks, updateFilter } from "@/redux/features/Tasks/taskSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
+  const dispatch = useAppDispatch();
   console.log(tasks);
 
   return (
     <div className="mx-auto max-w-7xl px-5 mt-20">
-      <div className="flex justify-between items-center">
-        <h1>Tasks</h1>
+      <div className="flex justify-end gap-5 items-center">
+        <h1 className="mr-auto">Tasks</h1>
+        <Tabs defaultValue="All">
+          <TabsList className="grid w-full grid-cols-4">
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("All"))}
+              value="All"
+            >
+              All
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("High"))}
+              value={Priority.High}
+            >
+              {Priority.High}
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("Medium"))}
+              value={Priority.Medium}
+            >
+              {Priority.Medium}
+            </TabsTrigger>
+            <TabsTrigger
+              onClick={() => dispatch(updateFilter("Low"))}
+              value={Priority.Low}
+            >
+              {Priority.Low}
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
         <AddTasksModel />
       </div>
       <div className="space-y-5 mt-5">
--- src/redux/features/Tasks/taskSlice.ts
+++ src/redux/features/Tasks/taskSlice.ts
@@ -1,3 +1,4 @@
+import { Priority } from "@/contants/priorityOptions";
 import { RootState } from "@/redux/store";
 import { ITask } from "@/types";
 import { createSlice, nanoid, PayloadAction } from "@reduxjs/toolkit";
@@ -17,7 +18,7 @@ const initialState: InitialState = {
       dueDate: "2025-01-14T18:00:00.000Z",
     },
   ],
-  filter: "Medium",
+  filter: "All",
 };
 
 type DraftTask = Pick<ITask, "title" | "description" | "dueDate" | "priority">;
@@ -49,10 +50,28 @@ export const taskSlice = createSlice({
     deleteTask: (state, action: PayloadAction<string>) => {
       state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
+    updateFilter: (
+      state,
+      action: PayloadAction<"All" | "High" | "Medium" | "Low">
+    ) => {
+      state.filter = action.payload;
+    },
   },
 });
 
-export const selectTasks = (state: RootState) => state.todo.tasks;
+export const selectTasks = (state: RootState) => {
+  const filter = state.todo.filter;
+  if (filter === Priority.Low) {
+    return state.todo.tasks.filter((task) => task.priority === "Low");
+  } else if (filter === Priority.Medium) {
+    return state.todo.tasks.filter((task) => task.priority === "Medium");
+  } else if (filter === Priority.High) {
+    return state.todo.tasks.filter((task) => task.priority === "High");
+  } else {
+    return state.todo.tasks;
+  }
+};
 export const selectFilter = (state: RootState) => state.todo.filter;
-export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask, updateFilter } =
+  taskSlice.actions;
 export default taskSlice.reducer;
